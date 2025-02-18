f4a918b108c3e7348657fff36c5169903e382e07
amberdhama <fauxcade007@gmail.com>
2025-02-18 12:57:37 +0530

    idk

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -2,7 +2,7 @@ const styles = {
   innerWidth: '2xl:max-w-[1280px] w-full',
   interWidth: 'lg:w-[80%] w-[100%]',
 
-  paddings: 'sm:p-16 xs:p-8 px-6 py-12',
+  paddings: 'sm:p-16 xs:p-8 px-6 py-6',
   yPaddings: 'sm:py-16 xs:py-8 py-12',
   xPaddings: 'sm:px-16 px-6',
   topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
@@ -12,14 +12,15 @@ const styles = {
   flexStart: 'flex justify-start items-start',
   flexEnd: 'flex justify-end',
   navPadding: 'pt-[98px]',
+ 
 
   // hero section
   heroHeading:
-      'font-bold lg:text-[120px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
+      'font-bold font-serif lg:text-[100px] md:text-[90px] sm:text-[60px] text-[44px] lg:leading-[100.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase font-extrabold tracking-wide bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-400 drop-shadow-lg',
   heroSubHeading:
-      'font-bold lg:text-[50px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-purple-500',
+      ' lg:text-[40px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[100.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] text-purple-500 gap-10 ' ,
     heroSubHeadingWhite:
-        'font-bold lg:text-[50px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
+        ' lg:text-[40px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px]  text-white',
 };
 
 export default styles;
