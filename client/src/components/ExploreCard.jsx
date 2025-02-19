ad7f9958a7789e1079968cb4811142b3f86fad45
Ishika <ishikachopra44@gmail.com>
2025-02-20 01:21:29 +0530

    added more responsiveness to explore

--- client/src/components/ExploreCard.jsx
+++ client/src/components/ExploreCard.jsx
@@ -23,7 +23,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         {title}
       </h3>
     ) : (
-      <div className="absolute bottom-0 p-8 flex justify-start w-full h-full flex-col bg-[rgba(0,0,0,0.5)] rounded-[24px]">
+      <div className="absolute bottom-0 p-8 flex justify-start w-full h-full md:h-2/5 flex-col bg-[rgba(0,0,0,0.5)] rounded-[24px] md:rounded-t-[0px]">
         <div
           className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
         >
